bb1cba20812162168305e0c1cb0077ccc9b0b174
seth-roychan <54307835+seth-roychan@users.noreply.github.com>
2020-01-10 17:54:37 +0800

    Completed Menu

--- src/router/index.js
+++ src/router/index.js
@@ -51,16 +51,64 @@ export const constantRoutes = [
       path: 'dashboard',
       name: 'Dashboard',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      meta: { title: '統計訊息', icon: 'dashboard' }
     }]
   },
 
   {
-    path: '/example',
+    path: '/sales',
     component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
+    children: [
+      {
+        path: 'index',
+        name: 'Sales',
+        component: () => import('@/views/sales/index'),
+        meta: { title: '銷售管理', icon: 'form' }
+      }
+    ]
+  },
+  {
+    path: '/purchase',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Purchase',
+        component: () => import('@/views/purchase/index'),
+        meta: { title: '採購管理', icon: 'form' }
+      }
+    ]
+  },
+  {
+    path: '/inventory',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Inventory',
+        component: () => import('@/views/inventory/index'),
+        meta: { title: '庫存管理', icon: 'form' }
+      }
+    ]
+  },
+  {
+    path: '/account',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Account',
+        component: () => import('@/views/account/index'),
+        meta: { title: '結算賬目', icon: 'form' }
+      }
+    ]
+  },
+  {
+    path: '/report',
+    component: Layout,
+    redirect: '/report',
+    name: 'Report',
+    meta: { title: '報表分析', icon: 'example' },
     children: [
       {
         path: 'table',
@@ -76,88 +124,219 @@ export const constantRoutes = [
       }
     ]
   },
-
   {
-    path: '/form',
+    path: '/setting',
     component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
-  },
-
-  {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
+    redirect: '/setting/customer',
+    name: 'Setting',
     meta: {
-      title: 'Nested',
+      title: '基本設定',
       icon: 'nested'
     },
     children: [
       {
-        path: 'menu1',
+        path: 'customermenu',
         component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
+        name: 'customermenu',
+        meta: { title: '客戶管理' },
         children: [
           {
-            path: 'menu1-1',
+            path: 'custometype',
             component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
+            name: 'customertype',
+            meta: { title: '客戶類別' }
           },
           {
-            path: 'menu1-2',
+            path: 'paymentterms',
             component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
+            name: 'paymentterms',
+            meta: { title: '付款設定' }
           },
           {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
+            path: 'paymentmethod',
+            component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+            name: 'paymentmethod',
+            meta: { title: '付款方式' }
+          },
+          {
+            path: 'region',
+            component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+            name: 'region',
+            meta: { title: '區域設定' }
           }
-        ]
+          ]},
+      {
+        path: 'vendormenu',
+        component: () => import('@/views/nested/menu1/menu1-3'),
+        name: 'VendorMenu',
+        meta: { title: '供應商管理' },
+        children: [
+          {
+            path: 'vendortype',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'vendortype',
+            meta: { title: '供應商類別' }
+          },
+          {
+            path: 'vendor',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'vendor',
+            meta: { title: '供應商設定' }
+          },
+          {
+            path: 'paymentterms',
+            component: () => import('@/views/nested/menu1/menu1-2'),
+            name: 'paymentterms',
+            meta: { title: '付款設定' }
+          },
+          {
+            path: 'paymentmethod',
+            component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+            name: 'paymentmethod',
+            meta: { title: '付款方式' }
+          }]
       },
       {
-        path: 'menu2',
+        path: 'usermenu',
         component: () => import('@/views/nested/menu2/index'),
-        meta: { title: 'menu2' }
-      }
-    ]
+        name: 'UserMenu',
+        meta: { title: '用戶管理' },
+        children: [
+          {
+            path: 'user',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'user',
+            meta: { title: '用戶設定' }
+          },
+          {
+            path: 'usergroup',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'usergroup',
+            meta: { title: '用戶群組' }
+          }]
+      },
+      {
+        path: 'productmenu',
+        component: () => import('@/views/nested/menu2/index'),
+        name: 'ProductMenu',
+        meta: { title: '貨物管理' },
+        children: [
+          {
+            path: 'customtype1',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'customtype1',
+            meta: { title: '自訂分類一' }
+          },
+          {
+            path: 'customtype2',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'customtype2',
+            meta: { title: '自訂分類二' }
+          },
+          {
+            path: 'customtype3',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'customtype3',
+            meta: { title: '自訂分類三' }
+          },
+          {
+            path: 'product',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'product',
+            meta: { title: '貨物設定' }
+          },
+          {
+            path: 'warehous',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'warehouse',
+            meta: { title: '倉庫設定' }
+          }]
+      },
+      {
+        path: 'contentmenu',
+        component: () => import('@/views/nested/menu2/index'),
+        name: 'ContentMenu',
+        meta: { title: '內容相關設定' },
+        children: [
+          {
+            path: 'currency',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'currency',
+            meta: { title: '貨幣設定' }
+          },
+          {
+            path: 'customtype2',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'customtype2',
+            meta: { title: '沖賬方式' }
+          },
+          {
+            path: 'customtype3',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'customtype3',
+            meta: { title: '銀行設定' }
+          },
+          {
+            path: 'product',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'product',
+            meta: { title: '賬戶設定' }
+          },
+          {
+            path: 'warehous水',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'warehouse',
+            meta: { title: '常用項目設定' }
+          }]
+      },
+      {
+        path: 'parametermenu',
+        component: () => import('@/views/nested/menu2/index'),
+        name: 'ContentMenu',
+        meta: { title: '系統參數設定' },
+        children: [
+          {
+            path: 'currency',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'currency',
+            meta: { title: '糸統編號設定' }
+          },
+          {
+            path: 'currency',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'currency',
+            meta: { title: '屬性偏好設定' }
+          },
+          {
+            path: 'currency',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'currency',
+            meta: { title: '印表機設定' }
+          },
+          {
+            path: 'currency',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'currency',
+            meta: { title: '檔案路徑設定' }
+          }]
+        }]
   },
 
   {
-    path: 'external-link',
+    path: '/setting',
     component: Layout,
     children: [
       {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
-      }
-    ]
+        path: 'index',
+        name: 'Setting',
+        component: () => import('@/views/setting/index'),
+        meta: { title: '進階功能', icon: 'form' },
+
+      }]
   },
+
+
+
 
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
